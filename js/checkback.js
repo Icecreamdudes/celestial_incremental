37b9b304ccb8b6e61fc3046d47cdff61952d8881
ResidualToast <residualToast@gmail.com>
2025-08-27 16:05:09 -0500

    actually set these variables, not just do nothing with the operation

--- js/checkback.js
+++ js/checkback.js
@@ -580,7 +580,7 @@
         }
 
         for (let i = 0; i < player.pet.singularityButtonTimers.length; i++) {
-            player.pet.singularityButtonTimers[i].sub(time);
+            player.pet.singularityButtonTimers[i] = player.pet.singularityButtonTimers[i].sub(time);
         }
 
         // Daily Reward (Insane Face Evo)
@@ -598,7 +598,7 @@
         player.leg.legendaryGemTimer = player.leg.legendaryGemTimer.sub(time);
         player.leg.summonTimer = player.leg.summonTimer.sub(time);
         for (let i = 0; i < player.pet.legendaryPetAbilityCooldowns.length; i++) {
-            player.pet.legendaryPetAbilityCooldowns[i].sub(time);
+            player.pet.legendaryPetAbilityCooldowns[i].sub(time) = player.pet.legendaryPetAbilityCooldowns[i].sub(time);
         }
 
         //rocket

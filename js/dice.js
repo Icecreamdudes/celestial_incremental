d95315c8d8436f11e7730ebe44e28edf48f2c299
Forwaken <cbower7534@gmail.com>
2026-01-08 19:33:04 -0500

    Matos emergency escape

--- js/dice.js
+++ js/dice.js
@@ -153,7 +153,8 @@
         player.d.dicePointsMult = player.d.dicePointsMult.pow(player.co.cores.dice.effect[1])
 
         // DICE POINT EFFECT
-        player.d.dicePointsEffect = player.d.dicePoints.plus(1).log10().mul(0.1).add(1)
+        player.d.dicePointsEffect = player.d.dicePoints.add(1).log(10).mul(0.1).add(1)
+        if (player.d.dicePointsEffect.eq(NaN) || player.d.dicePointsEffect.lt(1)) player.d.dicePointsEffect = new Decimal(1)
 
         //----------------------------------------
 

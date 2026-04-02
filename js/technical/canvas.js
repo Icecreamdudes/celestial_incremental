954697c7ff074de5221aedd12158f4be0a2088a7
Forwaken <cbower7534@gmail.com>
2026-04-01 22:10:12 -0400

    bro, why did you start popping your head up you loser

--- js/technical/canvas.js
+++ js/technical/canvas.js
@@ -29,7 +29,19 @@ function drawTree() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	if (player) {
 		let tree = universes[player.universe]?.tree
-		if (layers[player.tab].innerNodes && typeof tree == "object") tree = tree.concat(layers[player.tab].innerNodes)
+		if (layers[player.tab].innerNodes) {
+			let inner = layers[player.tab].innerNodes
+			for (row in inner) {
+				for (thing in inner[row]) {
+					let layer = inner[row][thing]
+					if (tmp[layer].layerShown == true && tmp[layer].branches) {
+						for (branch in tmp[layer].branches) {
+							drawTreeBranch(layer, tmp[layer].branches[branch])
+						}
+					}
+				}
+			}
+		}
 		if (typeof tree !== "undefined") {
 			for (row in tree) {
 				for (thing in tree[row]) {

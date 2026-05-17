9347f5eb26a32bd00616a254b4883c163a5a35c4
Forwaken <cbower7534@gmail.com>
2026-05-17 02:41:13 -0400

    Small fix

--- js/AltU2/shipBattle.js
+++ js/AltU2/shipBattle.js
@@ -1766,6 +1766,9 @@ class SpaceArena {
                     this.lastBounceClick = now;
                     let rect = this.canvas.getBoundingClientRect();
                     this.ship.bounceTarget = { x: this.mouseX, y: this.mouseY };
+                    if (this.ship.bounceTarget.x == undefined) {
+                        this.ship.bounceTarget = {x: this.width / 2, y: this.height / 2}
+                    }
                 }
             }
 
@@ -1825,6 +1828,9 @@ class SpaceArena {
                     this.lastDashClick = now;
                     let rect = this.canvas.getBoundingClientRect();
                     this.ship.dashTarget = { x: this.mouseX, y: this.mouseY };
+                    if (this.ship.dashTarget.x == undefined) {
+                        this.ship.dashTarget = {x: this.width / 2, y: this.height / 2}
+                    }
                 }
             }
 

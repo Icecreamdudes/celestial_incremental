b582ad4081a4691f0ee97657e892f3eeee74678e
bumpy <125849993+sdbumpy@users.noreply.github.com>
2026-07-18 01:42:19 -0500

    debug thing

--- js/AltU2/shipBattle.js
+++ js/AltU2/shipBattle.js
@@ -1327,6 +1327,7 @@ class SpaceArena {
     }
 
     spawnArena() {
+        player.ir.debugThing = ""
         this.arenaDiv = document.createElement('div');
         this.arenaDiv.id = 'space-arena';
         Object.assign(this.arenaDiv.style, {
@@ -1422,6 +1423,7 @@ class SpaceArena {
                 pointerId: e.pointerId,
                 action: e.action,
             })
+            player.ir.debugThing += "<br><span style='color:yellow'>"+JSON.stringify(this.pointerTouches.get(e.pointerId))
         }
     };
     handlePointerMove = (e) => {
@@ -1438,6 +1440,7 @@ class SpaceArena {
     };
     handlePointerUp = (e) => {
         if (player.ir.menu == 0) this.pointerDown = false;
+        player.ir.debugThing += "<br><span style='color:red'>"+JSON.stringify(this.pointerTouches.get(e.pointerId))
         if (player.ir.mobileControls) this.pointerTouches.delete(e.pointerId);
     };
     handlePointerCancel = (e) => {

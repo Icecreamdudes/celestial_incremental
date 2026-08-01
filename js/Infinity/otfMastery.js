2fc626ef10910005c2b5a985063a2d738e7ac93a
STCFCYT <pspindler@gmail.com>
2026-07-31 23:55:43 -0500

    Fix typo in feature name from 'Otherworldy' to 'Otherworldly'

--- js/Infinity/otfMastery.js
+++ js/Infinity/otfMastery.js
@@ -1,5 +1,5 @@
 ﻿addLayer("om", {
-    name: "Otherworldy Feature Mastery", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Otherworldly Feature Mastery", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "OM", // This appears on the layer's node. Default is the id with the first letter capitalized
     universe: "U2",
     row: 1,

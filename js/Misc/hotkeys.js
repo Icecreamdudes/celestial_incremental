13590f88477466d74d63cacb6cddb68aa57d4c60
bumpy <125849993+sdbumpy@users.noreply.github.com>
2026-08-30 15:25:49 -0500

    e

--- js/Misc/hotkeys.js
+++ js/Misc/hotkeys.js
@@ -110,7 +110,7 @@ function keyTable(uniID) {
             hk = knownHotkeys.global[k]
             if(hk.layer == "settings")
                 table.push(["row",[
-                    formatKey(hk,layers[hk.layer]),
+                    formatKey(hk.key,layers[hk.layer]),
                             ["raw-html","<div style='width:300px;'>"+hk.description+"</div>"]
                     ],{'border-style':'solid','border-top':''}
                 ])            
@@ -120,7 +120,7 @@ function keyTable(uniID) {
                 hk = knownHotkeys.global[k]
                 if(hk.uni == u)
                     table.push(["row",[
-                        formatKey(hk,layers[hk.layer]),
+                        formatKey(hk.key,layers[hk.layer]),
                                 ["raw-html","<div style='width:300px;'>"+hk.description+"</div>"]
                         ],{'border-style':'solid','border-top':''}
                     ])            
@@ -160,31 +160,25 @@ function keyTable(uniID) {
 }
 
 
-function formatKey(hk, layer) {
+function formatKey(keyStr, layer) {
+
     let basicKeyStyle = {'font-family':'monospace','font-size':'16px','display':'block','width':'24px','height':'24px','color':'black','align-content':'center','border-style':'solid','margin':'4px','border-color':'rgba(0, 0, 0, 0.3)'}
     let keyStyle = layer.nodeStyle ? 
         {...basicKeyStyle,'background-color': layer.color,...readData(layer.nodeStyle)} :
         {...basicKeyStyle,'background-color': layer.color}
-    if(hk.color) keyStyle = {...keyStyle, 'background-color': hk.color}
-    if(hk.style) keyStyle = {...keyStyle, ...hk.style}
-    keyStyle['border-radius'] = '0px'
+    let keyIcons = []
+
     keyStyle.transform = ''
 
-
-    
-    let keyIcons = []
-    let keyStr = hk.key
     if(keyStr.startsWith('ctrl+'))
     {
-        keyIcons.push(["display-text","Ctrl", {...keyStyle, 'width': '48px'}])
-        keyStr = keyStr.slice(5)
+        keyIcons.push(["display-text","Ctrl", keyStyle])
     }
 
     if(keyStr.toLowerCase() !== keyStr)
     {
         keyIcons.push(["display-text","⇧", keyStyle])   
     }
-    keyIcons.push(["display-text",keyStr.toUpperCase(), keyStyle])
+    keyIcons.push(["display-text",hk.key.toUpperCase(), keyStyle])
     return ["row",keyIcons]
 }
-
